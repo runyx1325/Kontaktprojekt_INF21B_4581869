b3e2a04fc7f4337c6786427081db30cafc2a2966
runyx1325 <lucaburst11@gmail.com>
2022-06-21 09:39:27 +0200

    edit 2088

--- Projekt/Main.java
+++ Projekt/Main.java
@@ -6,17 +6,9 @@ import java.time.LocalDateTime;
 public class Main{
 
     public static void main(String[] args) {
-
         Service service = new Service();
-
         DataImport.dataImportFromFile(new File("D:\\Studium\\DHBW\\2. Theoriesemester\\Programmieren\\Projekt\\contacts2021.db.txt"), service);     //Home
         //DataImport.dataImportFromFile(new File("C:\\Studium\\ProgrammierenJava\\Projekt\\contacts2021.db.txt"));                                  //Bosch
-
-        //Umgang mit Argumenten
-        //personensuche, wenn:              --personensuche="Ila"
-        //ortssuche, wenn:                  --ortssuche="Markt"
-        //kontaktpersonen anzeigen, wenn:   --kontaktpersonen=1
-        //besucher, wenn:                   --besucher=1,"2021-05-15T14:16:00"
 
         if(args.length > 0){
             String arg = args[0];
@@ -24,12 +16,10 @@ public class Main{
             switch (arg.charAt(0)) {
                 case 'p' -> {
                     arg = arg.replace("personensuche=", "");
-                    arg = arg.substring(1, arg.length() - 1);
                     service.personSearch(arg);
                 }
                 case 'o' -> {
                     arg = arg.replace("ortssuche=", "");
-                    arg = arg.substring(1, arg.length() - 1);
                     service.locationSearch(arg);
                 }
                 case 'k' -> {
@@ -45,18 +35,6 @@ public class Main{
                 }
                 default -> System.out.println("This argument does not exist.");
             }
-        }else {
-            //System.out.println(service.showContactPersons(66));
-            System.out.println(service.visitor(1,LocalDateTime.parse("2021-05-15T14:16:00")));
         }
-
-
-
-
-
-
-
-
-
     }
 }
